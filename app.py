cfe474c07f7394159d3bcb2596891fc2bf0c1a50
Lando Ledesma <lando_ledesma@outlook.com>
2023-10-04 16:04:43 -0600

    creando diapositivas pero hay un bug con la informacion

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ import base64
 import os 
 from dotenv import load_dotenv
 
+# Cargando variables de entorno
+load_dotenv("token.env")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 # format options
 TITLE_FONT_SIZE = Pt(30)
 SLIDE_FONT_SIZE = Pt(16)
@@ -24,33 +29,41 @@ def slide_content(slide_title):
     response = openai.Completion.create(
         engine = "text-davinci-003",
         prompt = prompt,
-        max_tokens = 500
+        max_tokens = 1000
     )
     return response['choices'][0]['text']
 
 def create_presentation(topic,slide_titles,slide_contents):
+    if not os.path.exists('generated_ppt'):
+        os.makedirs('generated_ppt')
     prs = pptx.Presentation()
-    slide_layout = prs.slide_layout[1]
+    
+    slide_layout = prs.slide_layouts[1]
 
-    title_slide = prs.slides.add_slide(prs.slide_layout[0])
+    title_slide = prs.slides.add_slide(prs.slide_layouts[0])
     title_slide.shapes.title.text = topic 
     
     for slide_title,slide_content in zip(slide_titles,slide_contents):
         slide = prs.slides.add_slide(slide_layout)
         slide.shapes.title.text = slide_title 
-        slide.sahpes.placeholder[1].text = slide_content
+        slide.shapes.placeholders[1].text = slide_content
 
-        slide.sahpes.title.text_frame.paragraphs[0].font.size = TITLE_FONT_SIZE
+        slide.shapes.title.text_frame.paragraphs[0].font.size = TITLE_FONT_SIZE
         for shape in slide.shapes:
             if shape.has_text_frame:
                 text_frame = shape.text_frame
-                for paragraph in text_frame.paragraph:
+                for paragraph in text_frame.paragraphs:
                     paragraph.font.size = SLIDE_FONT_SIZE
-    prs.save(f"ppt/{topic}_presentacion.pptx")
+    prs.save(f"generated_ppt/{topic}_presentation.pptx")
+
 
 
 def download_link(topic):
+    # Verificar y crear el directorio 'generated_ppt' si no existe
+    if not os.path.exists('generated_ppt'):
+        os.makedirs('generated_ppt')
     ppt_filename = f"generated_ppt/{topic}_presentation.pptx"
+
 
     with open(ppt_filename, "rb") as file:
         ppt_contents = file.read()
@@ -67,7 +80,7 @@ def main():
 
     if generate_button and topic:
         st.info("Generando presentacion... Por favor espera .")
-        slide_titles = slide_titles(topic)
+        slide_titles = slide_title(topic)
         filtered_slide_titles= [item for item in slide_titles if item.strip() != '']
         print("Titulo: ", filtered_slide_titles)
         slide_contents = [slide_content(title) for title in filtered_slide_titles]
